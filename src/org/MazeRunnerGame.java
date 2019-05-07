0e2779453f593ccdc81dddda4b96e5f335a3bcb6
Kameron_Gilham <Kameron_Gilham@NoremakPC>
2019-05-07 03:02:34 -0600

    Commit #3

--- src/org/MazeRunnerGame.java
+++ src/org/MazeRunnerGame.java
@@ -2,55 +2,94 @@ package org;
 
 import javafx.animation.AnimationTimer;
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.Button;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 public class MazeRunnerGame extends Application {
-	
-	int canvasW= 800;
+
+	int canvasW = 800;
 	int canvasH = 800;
+	static boolean gameRunning;
 	Runner runner;
-	
+	String message;
+
+	public MazeRunnerGame() {
+		gameRunning = false;
+		message = "Press Start to Begin";
+	}
+
 	@Override
 	public void start(Stage stage) throws Exception {
+
 		Canvas canvas = new Canvas(canvasW, canvasH);
 		GraphicsContext gc = canvas.getGraphicsContext2D();
 		KeyboardInputs keyboarding = new KeyboardInputs();
 
+		Button startButton = new Button("Start");
+		startButton.setOnAction( e -> {message = "";});
+		startButton.setOnAction(e -> {gameRunning = true;});
+		Button quitButton = new Button("Quit");
+		quitButton.setOnAction( e -> Platform.exit() );
+
+		HBox buttonBar = new HBox(canvasW / 3, startButton, quitButton);
+		buttonBar.setAlignment(Pos.BOTTOM_CENTER);
+
 		BorderPane game = new BorderPane();
-		Scene scene = new Scene(game, canvasW, canvasH, Color.BLACK);
+		game.setBottom(buttonBar);
+
+		Scene scene = new Scene(game, canvasW, canvasH + 80, Color.WHITE);
 		scene.addEventHandler(KeyEvent.KEY_PRESSED, (key) -> keyboarding.keyboardInput(key, gc));
 		scene.addEventHandler(KeyEvent.KEY_RELEASED, (key) -> keyboarding.keyboardInput(key, gc));
 		game.setCenter(canvas);
 		stage.setScene(scene);
 		stage.setTitle("MazeRunner");
 		stage.show();
+		
+		gc.setFont(Font.font(50));
+		gc.setLineWidth(5.0);
+		gc.setFill(Color.BLACK);
+		gc.fillText(message, canvasW/5, canvasH/2);
 
 		new AnimationTimer() {
 
 			@Override
 			public void handle(long now) {
 
-				gc.clearRect(0, 0, gc.getCanvas().getWidth(), gc.getCanvas().getHeight());
-				runner.update(keyboarding, gc);
-				runner.draw(gc);
+				if (gameRunning) {
 
+					gc.clearRect(0, 0, gc.getCanvas().getWidth(), gc.getCanvas().getHeight());
+					runner.update(keyboarding, gc);
+					runner.draw(gc);
+					gc.setFont(Font.font(canvasH / 10));
+					gc.setStroke(Color.BLUE);
+					gc.strokeText("Start", canvasW / 10, canvasH / 10);
+					gc.setStroke(Color.GREEN);
+					gc.strokeText("End", canvasW * 8 / 10, canvasH * 9 / 10);
+					
+					
+
+				}
 			}
 
 		}.start();
 
 	}
+
 	@Override
 	public void init() {
-		
-		runner = new Runner(canvasW/20, canvasH/20, canvasW/20, canvasH/20, Color.TAN);
-		
-		
+
+		runner = new Runner(canvasW / 20, canvasH / 20, canvasW / 20, canvasH / 20, Color.TAN);
+
 	}
 
 }
--- src/org/Runner.java
+++ src/org/Runner.java
@@ -5,15 +5,13 @@ import javafx.scene.paint.Color;
 
 public class Runner {
 	Color runnerColor;
-	boolean gameRunning;
-	static int runnerX;
-	static int runnerY;
-	static int runnerW;
-	static int runnerH;
+	int runnerX;
+	int runnerY;
+	int runnerW;
+	int runnerH;
 	int runnerFrames;
 
 	public Runner(int x, int y, int w, int h, Color color) {
-		gameRunning = true;
 		runnerX = x;
 		runnerY = y;
 		runnerW = w;
@@ -31,38 +29,34 @@ public class Runner {
 	}
 
 	public void update(KeyboardInputs keyboarding, GraphicsContext gc) {
-		
-		if (gameRunning) {
 
-			if (keyboarding.up) {
-				if (runnerY > runnerH / 2) {
-					runnerY--;
-				} else {
-					keyboarding.up = false;
-				}
+		if (keyboarding.up) {
+			if (runnerY > runnerH / 2) {
+				runnerY--;
+			} else {
+				keyboarding.up = false;
 			}
-			if (keyboarding.down) {
-				if (runnerY < gc.getCanvas().getHeight() - runnerH / 2) {
-					runnerY++;
-				} else {
-					keyboarding.down = false;
-				}
+		}
+		if (keyboarding.down) {
+			if (runnerY < gc.getCanvas().getHeight() - runnerH / 2) {
+				runnerY++;
+			} else {
+				keyboarding.down = false;
 			}
-			if (keyboarding.left) {
-				if (runnerX > runnerW / 2) {
-					runnerX--;
-				} else {
-					keyboarding.left = false;
-				}
+		}
+		if (keyboarding.left) {
+			if (runnerX > runnerW / 2) {
+				runnerX--;
+			} else {
+				keyboarding.left = false;
 			}
-			if (keyboarding.right) {
-				if (runnerX < gc.getCanvas().getWidth() - runnerW / 2) {
-					runnerX++;
-				} else {
-					keyboarding.right = false;
-				}
+		}
+		if (keyboarding.right) {
+			if (runnerX < gc.getCanvas().getWidth() - runnerW / 2) {
+				runnerX++;
+			} else {
+				keyboarding.right = false;
 			}
-
 		}
 
 	}
